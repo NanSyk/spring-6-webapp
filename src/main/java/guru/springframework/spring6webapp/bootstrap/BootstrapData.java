719b23c8acf0ae285f80f54d11f30b500c46e05f
nam <nguyensynam19021999@gmail.com>
2024-06-23 08:48:56 +0900

    Assignment#1

--- src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring6webapp/bootstrap/BootstrapData.java
@@ -2,20 +2,27 @@ package guru.springframework.spring6webapp.bootstrap;
 
 import guru.springframework.spring6webapp.domain.Author;
 import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Publisher;
 import guru.springframework.spring6webapp.repositories.AuthorRepository;
 import guru.springframework.spring6webapp.repositories.BookRepository;
+import guru.springframework.spring6webapp.repositories.PublisherRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BootstrapData implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(BootstrapData.class);
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootstrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -28,8 +35,16 @@ public class BootstrapData implements CommandLineRunner {
         ddd.setTitle("Domain Driven Design");
         ddd.setIsbn("123456");
 
+        Publisher addisonWesley = new Publisher();
+        addisonWesley.setPublisherName("Addison-Wesley Professional");
+        addisonWesley.setCity("America");
+        addisonWesley.setState("CA");
+        addisonWesley.setZip("95472");
+        addisonWesley.setAddress("1005 Gravenstein Highway North Sebastopol, CA 95472");
+
         Author ericSaved = authorRepository.save(eric);
         Book dddSaved = bookRepository.save(ddd);
+        Publisher addisonWesleySaved = publisherRepository.save(addisonWesley);
 
         Author rod = new Author();
         rod.setFirstName("Rod");
@@ -39,19 +54,32 @@ public class BootstrapData implements CommandLineRunner {
         noEJB.setTitle("J2EE Development without EJB");
         noEJB.setIsbn("3254891");
 
+        Publisher wrox = new Publisher();
+        wrox.setPublisherName("Wrox");
+        wrox.setCity("England");
+        wrox.setState("Birmingham");
+        wrox.setZip("?????");
+        wrox.setAddress("????????????????????");
+
         Author rodSaved = authorRepository.save(rod);
         Book noEJBSaved = bookRepository.save(noEJB);
+        Publisher wroxSaved = publisherRepository.save(wrox);
 
         ericSaved.getBooks().add(dddSaved);
         rodSaved.getBooks().add(noEJBSaved);
+        addisonWesleySaved.getBooks().add(dddSaved);
+        wroxSaved.getBooks().add(noEJBSaved);
 
         authorRepository.save(ericSaved);
         authorRepository.save(rodSaved);
         bookRepository.save(dddSaved);
         bookRepository.save(noEJBSaved);
+        publisherRepository.save(addisonWesleySaved);
+        publisherRepository.save(wroxSaved);
 
         System.out.println("In Bootstrap");
         System.out.println("Book count: " + bookRepository.count());
         System.out.println("Author count: " + authorRepository.count());
+        System.out.println("Publisher count: " + publisherRepository.count());
     }
 }
